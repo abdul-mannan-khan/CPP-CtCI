d5738ad0ec6c79367091b8f106fbd90265c1d40f
abdul-mannan-khan <abdul.mannan.617@gmail.com>
2024-01-31 20:16:27 +0000

    Added Comments

--- arrays_string/rotate_matrix.cpp
+++ arrays_string/rotate_matrix.cpp
@@ -1,13 +1,14 @@
 /*
- * Cracking the coding interview edition 6
- * Problem 1.7 Rotate a matrix by 90' clockwise ( or anticlockwise).
- * Solution : I have done it two ways.
+ * Book: Cracking the coding interview, edition 6 
+ * Problem 1.7: Rotate a matrix by 90' clockwise ( or anticlockwise).
  * Approach 1: Take transpose of matrix and then reverse the rows for clockwise 90' rotation.
  * 			   Obviously if we reverse the columns we will get anticlockwise 90' rotation.
  * Approach 2: As mentioned in the book, rotating invididual elements layer by layer.
  * 			   I have solved it perform anticlockwise 90' rotation, it can be done similarly for clockwise rotatation.
  */
 #include<iostream>
+
+using namespace std;
 
 void helper_transpose(int **matrix, int N)
 {
@@ -80,5 +81,7 @@ int main() {
 	std::cout << "Rotated matrix again by 90(anticlockwise):\n";
 	rotate2(matrix, N);
 	printMatrix(matrix, N);
+
+    // This is an arbirary comment to make a commit
 	return 0;
 }
